c30955f6b410042bbba904af90f2aad4e6ae68b1
moomoopal <40549274+moomoopal@users.noreply.github.com>
2018-07-14 17:30:27 -0700

    First
    Button, Light, and Buzzer

---
+++ FencingRobot1.ino
@@ -0,0 +1,37 @@
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(2,OUTPUT);
+  pinMode(3,INPUT);
+
+  digitalWrite(2,HIGH);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+
+  if (digitalRead(2)==HIGH) {
+    // light is on
+    if(digitalRead(3)==LOW){
+      tone(5,440);
+      delay(1000);
+      digitalWrite(2,LOW);
+      noTone(5);
+    }
+    else{
+      noTone(5);
+    }
+  }
+  else {
+    // light is off
+    
+
+  
+  }
+  
+  
+
+
+  
+
+}
