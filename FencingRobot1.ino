8ddb3631ab2fba08e7e9c3c65e02676857337400
moomoopal <40549274+moomoopal@users.noreply.github.com>
2018-07-16 20:02:27 -0700

    Buzzer Timing
    Buzzers on knee and foot, added the ability to process button while lights are blinking

--- FencingRobot1.ino
+++ FencingRobot1.ino
@@ -1,15 +1,13 @@
 void setup() {
   // put your setup code here, to run once:
-  pinMode(2,OUTPUT);
+  pinMode(2,OUTPUT);  
   pinMode(3,INPUT);
-
-  digitalWrite(2,HIGH);
+  pinMode(4,OUTPUT);
+  pinMode(7,INPUT);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-
-
+void processButton(){
+  
   if (digitalRead(2)==HIGH) {
     // light is on
     if(digitalRead(3)==LOW){
@@ -24,14 +22,48 @@ void loop() {
   }
   else {
     // light is off
-    
+  }
 
+  if (digitalRead(4)==HIGH) {
+    // light is on
+    if(digitalRead(7)==LOW){
+      tone(5,440);
+      delay(1000);
+      digitalWrite(4,LOW);
+      noTone(5);
+    }
+    else{
+      noTone(5);
+    }
+  }
+  else {
+    // light is off
+  }
   
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+  //light combination
+
+  digitalWrite(2,HIGH);
+  digitalWrite(4,HIGH);
+  for(int i=0; i<15151; i++){
+    processButton(); 
+  }
+  digitalWrite(2,LOW);
+  for(int i=0; i<15151; i++){
+    processButton(); 
   }
   
   
-
-
+  
+  }
   
 
-}
+  //code below this line = buzzing
+  
+  
+  
+
