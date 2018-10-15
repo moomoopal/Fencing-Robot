a2b8013e6a477638300c491f2355db529cc51e69
moomoopal <40549274+moomoopal@users.noreply.github.com>
2018-10-14 17:53:21 -0700

    DC Driver Commands and speed and time

--- FencingRobotCar.ino
+++ FencingRobotCar.ino
@@ -1,20 +1,112 @@
+int startButton = 2;
 int positiveLeftMotor = 3;
 int negativeLeftMotor = 5;
 int positiveRightMotor = 6;
 int negativeRightMotor = 9;
 
+// DC Motor Car Functions
+void moveLeftForward(int);
+void moveRightForward(int);
+void moveBothForward(int, int);
+void moveLeftBackward(int);
+void moveRightBackward(int);
+void moveBothBackward(int, int); 
+void rStopLeft();
+void rStopRight();
+void rStopBoth();
+void moveHalfLeftForward();
+void waitForButton();
+
 void setup() {
   // put your setup code here, to run once:
-  
+  pinMode (startButton, INPUT);
   pinMode (positiveLeftMotor,OUTPUT);
   pinMode (negativeLeftMotor,OUTPUT);
   pinMode (positiveRightMotor,OUTPUT);
   pinMode (negativeRightMotor,OUTPUT);
+  Serial.begin(9600);
 }
 
+void moveLeftForward(int pwr) {
+  analogWrite (positiveLeftMotor,pwr);
+  analogWrite (negativeLeftMotor, 0);
+  return;
+}
+
+void moveRightForward(int pwr) {
+  analogWrite (positiveRightMotor,pwr);
+  analogWrite (negativeRightMotor,0);
+  return;
+}
+
+void moveBothForward(int ms, int pwr) {
+  moveLeftForward(pwr);
+  moveRightForward(pwr);
+  delay(ms);
+  rStopBoth();
+  return;
+}
+
+void moveLeftBackward(int pwr){
+  analogWrite (positiveLeftMotor,0);
+  analogWrite (negativeLeftMotor,pwr); 
+  return;
+}
+
+void moveRightBackward(int pwr){
+  analogWrite (positiveRightMotor,0);
+  analogWrite (negativeRightMotor,pwr);
+  return;
+}
+
+void moveBothBackward(int ms, int pwr){
+  moveLeftBackward(pwr);
+  moveRightBackward(pwr);
+  delay(ms);
+  rStopBoth();
+  return;
+}
+
+void rStopLeft(){
+  digitalWrite (positiveLeftMotor,LOW);
+  digitalWrite (negativeLeftMotor,LOW);
+  return;
+}
+
+void rStopRight(){
+  digitalWrite (positiveRightMotor,LOW);
+  digitalWrite (negativeRightMotor,LOW);
+  return;
+}
+
+void rStopBoth(){
+  rStopLeft();
+  rStopRight();
+  return;
+}
+
+void moveHalfLeftForward(){
+  analogWrite (positiveLeftMotor, 80);
+  analogWrite (negativeLeftMotor, 0);
+}
+void waitForButton(){
+  int buttonPress;
+  buttonPress = digitalRead(startButton);
+  while (buttonPress == HIGH){
+    //Serial.println(buttonPress); 
+    buttonPress = digitalRead(startButton);
+  }
+  return;
+}
 void loop() {
   // put your main code here, to run repeatedly:
-  digitalWrite (positiveLeftMotor,HIGH);
+  waitForButton();
+  moveBothForward(1000, 250);
+  delay(1000);
+  moveBothBackward(1000, 170);
+  delay(1000);
+  
+  /*digitalWrite (positiveLeftMotor,HIGH);
   digitalWrite (negativeLeftMotor,LOW);
   delay(1000);
   digitalWrite (positiveLeftMotor,LOW);
@@ -39,4 +131,7 @@ void loop() {
   digitalWrite (positiveRightMotor,HIGH);
   digitalWrite (negativeRightMotor,HIGH);
   delay(500);
+  */
+
+  
 }
