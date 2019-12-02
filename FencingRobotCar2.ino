c8da109b5aacd431d0fb4254c02238f9f8ffc985
Natalie Leung <40549274+polarcow285@users.noreply.github.com>
2019-12-01 16:39:13 -0800

    IR remote
    Added IR remote
    Moved pins for positiveLeftMotor and positiveRightMotor because of timers

---
+++ FencingRobotCar2.ino
@@ -0,0 +1,309 @@
+#include <IRremote.h>
+int receiver_pin = 7;  
+IRrecv receiver(receiver_pin); 
+decode_results output;
+
+#define LMOTORADJ 33
+#define RMOTORADJ 36
+int startButton = 2;
+int positiveLeftMotor = 5;
+int negativeLeftMotor = 10;
+int positiveRightMotor = 6;
+int negativeRightMotor = 9;
+int echo = 12;
+int trigger = 13;
+
+// DC Motor Car Functions
+void moveLeftForward(int);
+void moveRightForward(int);
+void moveBothForward(int, int);
+void moveLeftBackward(int);
+void moveRightBackward(int);
+void moveBothBackward(int, int); 
+void moveBothForwardContinuously(int);
+void moveBothBackwardContinuously(int);
+void rStopLeft();
+void rStopRight();
+void rStopBoth();
+void moveHalfLeftForward();
+void waitForButton();
+int distanceMonitor();
+void wallDetector();
+void keepingDistance(int);
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode (startButton, INPUT);
+  pinMode (positiveLeftMotor,OUTPUT);
+  pinMode (negativeLeftMotor,OUTPUT);
+  pinMode (positiveRightMotor,OUTPUT);
+  pinMode (negativeRightMotor,OUTPUT);
+  pinMode (echo, INPUT);
+  pinMode (trigger, OUTPUT);
+  Serial.begin(9600);
+  receiver.enableIRIn();
+}
+
+void moveLeftForward(int pwr) {
+  analogWrite (positiveLeftMotor,pwr);
+  analogWrite (negativeLeftMotor, 0);
+  return;
+}
+
+void moveRightForward(int pwr) {
+  analogWrite (positiveRightMotor,pwr);
+  analogWrite (negativeRightMotor,0);
+  return;
+}
+
+void moveBothForward(int ms, int pwr) {
+  moveLeftForward(pwr);
+  //0.5 Voltage difference between the two motors, multiply by 100 and add to right (weaker) pwr to make the robot go straight
+  moveRightForward(pwr+RMOTORADJ);
+  delay(ms);
+  rStopBoth();
+  return;
+}
+//caliberated using 5.8 volts on DC driver
+void moveBothForwardCm(int cm, int pwr) {
+  int ms;
+  float spd;
+  if (pwr == 100){
+    spd = 0.02;
+    ms = cm/spd;
+    moveLeftForward(pwr);
+    //0.5 Voltage difference between the two motors, multiply by 100 and add to right (weaker) pwr to make the robot go straight
+    moveRightForward(pwr+RMOTORADJ);
+    delay(ms);
+    rStopBoth(); 
+  }
+  else if(pwr == 162){
+    spd = 0.027;
+    ms = cm/spd;
+    moveLeftForward(pwr);
+    //0.5 Voltage difference between the two motors, multiply by 100 and add to right (weaker) pwr to make the robot go straight
+    moveRightForward(pwr+RMOTORADJ);
+    delay(ms);
+    rStopBoth(); 
+  }
+  else if(pwr == 200){
+    spd = 0.036;
+    ms = cm/spd;
+    moveLeftForward(pwr);
+    //0.5 Voltage difference between the two motors, multiply by 100 and add to right (weaker) pwr to make the robot go straight
+    moveRightForward(pwr+RMOTORADJ);
+    delay(ms);
+    rStopBoth();
+    
+  }
+  
+ 
+  return;
+}
+void moveBothBackwardCm(int cm, int pwr) {
+  int ms;
+  float spd;
+  if (pwr == 100){
+    spd = 0.02;
+    ms = cm/spd;
+    moveLeftBackward(pwr+LMOTORADJ);
+    moveRightBackward(pwr);
+    delay(ms);
+    rStopBoth(); 
+     
+  }
+  else if(pwr == 162){
+    spd = 0.027;
+    ms = cm/spd;
+    moveLeftBackward(pwr+LMOTORADJ);
+    moveRightBackward(pwr);
+    delay(ms);
+    rStopBoth(); 
+  }
+  else if(pwr == 200){
+    spd = 0.036;
+    ms = cm/spd;
+    moveLeftBackward(pwr+LMOTORADJ);
+    moveRightBackward(pwr);
+    delay(ms);
+    rStopBoth(); 
+  }
+  
+ 
+  return;
+}
+
+void moveLeftBackward(int pwr){
+  analogWrite (positiveLeftMotor,0);
+  analogWrite (negativeLeftMotor,pwr); 
+  return;
+}
+
+void moveRightBackward(int pwr){
+  analogWrite (positiveRightMotor,0);
+  analogWrite (negativeRightMotor,pwr);
+  return;
+}
+void moveBothBackwardContinuously(int pwr){
+  moveLeftBackward(pwr+LMOTORADJ);
+  moveRightBackward(pwr);
+  return;
+}
+void moveBothBackward(int ms, int pwr){
+  moveLeftBackward(pwr+LMOTORADJ);
+  moveRightBackward(pwr);
+  delay(ms);
+  rStopBoth();
+  return;
+}
+
+void rStopLeft(){
+  digitalWrite (positiveLeftMotor,LOW);
+  digitalWrite (negativeLeftMotor,LOW);
+  return;
+}
+
+void rStopRight(){
+  digitalWrite (positiveRightMotor,LOW);
+  digitalWrite (negativeRightMotor,LOW);
+  return;
+}
+
+void rStopBoth(){
+  rStopLeft();
+  rStopRight();
+  return;
+}
+
+void moveHalfLeftForward(){
+  analogWrite (positiveLeftMotor, 80);
+  analogWrite (negativeLeftMotor, 0);
+}
+void moveBothForwardContinuously(int pwr){
+  moveLeftForward(pwr);
+  //0.5 Voltage difference between the two motors, multiply by 100 and add to right (weaker) pwr to make the robot go straight
+  moveRightForward(pwr+50);
+}
+void waitForButton(){
+  int buttonPress;
+  buttonPress = digitalRead(startButton);
+  while (buttonPress == HIGH){
+    //Serial.println(buttonPress); 
+    buttonPress = digitalRead(startButton);
+  }
+  return;
+}
+void wallDetector(){
+  long distance;
+  waitForButton();
+
+  while (1){
+    
+    distance = distanceMonitor();
+    if (distance != 0) {
+      Serial.println(distance);
+    }
+    else{
+      //do nothing 
+    }
+    
+    //1>= distance <=10
+    if ((distance >=1)&&(distance <=35)){
+      rStopBoth();
+    }
+    else{
+      moveBothForwardContinuously(150);
+    }
+    
+  }
+}
+
+void keepingDistance(int fencingDistance){
+  long distance;
+  int spd = 200;
+  long tolerance = 3;
+  long delta;
+  waitForButton();
+  
+  while (1){
+    
+    distance = distanceMonitor();
+    delay(100);
+    Serial.println(distance);
+    delta = abs(distance - fencingDistance);
+    if (delta > tolerance){
+      //if the distance is far
+      if (distance > fencingDistance){
+        moveBothForwardCm(delta, spd);
+      }
+      //1>= distance <=10
+      //if distance is close
+      if ((distance >=1)&&(distance <= fencingDistance)){
+        moveBothBackwardCm(delta, spd);
+      }
+      else{
+        //if we see a 0, it stops
+        rStopBoth();
+      }
+      
+    }
+    else{
+      rStopBoth();
+    }
+    
+  }
+}
+
+void loop() {
+  //waitForButton();
+  //delay(1000);
+
+  if (receiver.decode(&output)){
+    unsigned int value = output.value;
+    if (value == 6375){
+      //moveBothForwardContinuously(162);
+      moveRightForward(162);
+    }
+    if (value == 19125){
+      moveBothBackwardContinuously(162);
+    }
+    if (value == 39015){
+      rStopBoth();
+    }
+    Serial.println(value);
+    Serial.println(analogRead(11));
+    receiver.resume();
+  }
+  //while (1){
+  //keepingDistance(15);
+  //}
+  
+  /*moveBothForwardCm(20, 162); 
+  delay(1000);
+  moveBothBackwardCm(10, 162);
+  delay(1000);
+  moveBothForwardCm(25, 162);
+  delay(1000);
+  moveBothBackwardCm(30, 162);
+  delay(1000);
+  */
+}
+
+int distanceMonitor(){
+  
+
+  long distance;
+  //send pulse
+  digitalWrite (trigger, LOW);
+  delayMicroseconds (2);
+  digitalWrite (trigger, HIGH);
+  delayMicroseconds (20);
+  digitalWrite (trigger, LOW);
+
+  //wait for pulse to come back
+  distance = pulseIn(echo, HIGH);
+  distance /= 59;
+  return distance;
+  
+  
+}
